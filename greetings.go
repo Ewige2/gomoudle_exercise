f5b2ebc2870bb2dfd2a75b0eb761d696fad339f4
ewige2 <2009660227@nustti.edu.cn>
2023-04-11 13:25:11 +0800

    v1.1.4

--- greetings.go
+++ greetings.go
@@ -2,7 +2,7 @@ package greetings
 
 import "fmt"
 
-// Hello 为指定的人返回问候语.
+// Hello 为指定的人返回问候语 .
 func Hello(name string) string {
 	// 返回在消息中嵌入名称的问候语.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
